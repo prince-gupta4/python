7cbfe920371482885bbd1d0d86c7de993f8244d5
Prince Gupta <princegupta7698@gmail.com>
2026-09-22 12:11:41 +0530

    Unit 3 programs

---
+++ unit3/3.py
@@ -0,0 +1,17 @@
+import math
+
+print(math.pi)
+
+nums = [10,12,24,75,34,23]
+print()
+
+math.pi = 25
+print(math.pi)
+print(math.sqrt(40))
+print(math.pow(2, 5))
+print(math.fabs(-56))
+print(math.floor(19.545))
+print(math.ceil(19.545))
+print(math.factorial(5))
+print(math.lcm(5,3,2,4,4,3,3))
+print(math.remainder(10, 3))
